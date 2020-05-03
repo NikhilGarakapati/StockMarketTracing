98371410b291b2698e9637cff706a20d6f9e970e
NikhilGarakapati <nikhilgarakapati@gmail.com>
2020-05-03 14:51:41 +0530

    refraction done!

--- src/resources/stock.js
+++ src/resources/stock.js
@@ -4,12 +4,37 @@ import { iex } from '../config/iex.js';
 export const stock = {
 
     latestPrice: (ticker, callback) => {
-        const url = `${iex.url}/stock/${ticker}/intraday-prices?&token=${iex.token}`;
-        
+        const url = stock.latestPriceURL(ticker);
         fetch(url)
             .then((response) => response.json())
-            .then((data) => {
-                callback(data)
-            })
+            .then((data) => callback(stock.formatPriceData(data)))
+            .catch(err => console.log(err));
+    },
+
+    latestPriceURL: (ticker)=>{
+        return `${iex.url}/stock/${ticker}/intraday-prices?token=${iex.token}`
+    },
+
+    formatPriceData: (data) => {
+        const stockData = data[data.length -1]
+        const formattedData = {}
+        formattedData.price = stockData.close
+        formattedData.date = stockData.date
+        formattedData.time = stockData.label 
+        return formattedData
+    },
+
+    getYesterdaysClose:(ticker, date, callback) =>{
+        const url = stock.getYesterdaysCloseURL(ticker, date);
+        fetch(url)
+            .then((response) => response.json())
+            .then((data) => callback(stock.formatPriceData(data)))
+            .catch(err => console.log(err));
+    },
+
+    getYesterdaysCloseURL: (ticker) =>{
+        return `${iex.url}/stock/${ticker}/intraday-prices?exactDate=20190501&token=${iex.token}`
+
     }
+
 }
